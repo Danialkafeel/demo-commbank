8e52d4a9a36c822772376a5c73a005b9ffea496f
danialkafee <danialkafee@adobe.com>
2024-07-11 14:01:39 +0530

    Adding blocks from block collection

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,42 +1,18 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  // Add the container class to the outer div
-  block.classList.add('cards-container');
-
-  // Get all cards
-  const cards = block.querySelectorAll('.cards > div');
-
-  // Add card class to each card
-  cards.forEach((card) => {
-    card.classList.add('card');
-
-    // Create image container
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('card-image-container');
-    
-    // Move image into the image container
-    const img = card.querySelector('picture');
-    imgContainer.appendChild(img);
-    
-    // Move image container to the top of the card
-    card.insertBefore(imgContainer, card.firstChild);
-
-    // Wrap all remaining content in a content container
-    const contentContainer = document.createElement('div');
-    contentContainer.classList.add('card-content-container');
-    while (imgContainer.nextSibling) {
-      contentContainer.appendChild(imgContainer.nextSibling);
-    }
-    card.appendChild(contentContainer);
-
-    // Beautify "Apply now" button
-    const applyNowLink = card.querySelector('p > a');
-    if (applyNowLink && applyNowLink.textContent.trim() === 'Apply now') {
-      const buttonContainer = document.createElement('div');
-      buttonContainer.classList.add('button-container');
-      buttonContainer.appendChild(applyNowLink);
-      card.appendChild(buttonContainer);
-      applyNowLink.classList.add('apply-now-button');
-    }
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
   });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
